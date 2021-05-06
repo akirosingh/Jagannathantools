9b17041d7e767983b428539f1214e79b063d428a
akirosingh <adamsk@stanford.edu>
2021-05-06 04:56:57 -0700

    Adding beefiness to the rename flowjo 🐄️

--- R/rename_flowjo.R
+++ R/rename_flowjo.R
@@ -4,15 +4,31 @@
 ##' @title Fixing the column names of csv files exported from flowjo
 ##' @param data dataframe from flowjo with raw column names
 ##' @param removal the parts of the column names to remove
+##' @param feature_num the number of additional information to column names
 ##' @return
 ##' @author akirosingh
 ##' @export
-rename_flowjo <- function(data, removal = " \\| Freq\\. of Parent \\(\\%\\)") {
+rename_flowjo <- function(data, feature_num = 1, removal = " \\| Freq\\. of Parent \\(\\%\\)") {
+  renamer <-function(name, feature_num){
+    catch <- basename(name)
+    for (i in 1:(feature_num-1)){
+      output<- dirname(name)
+      catch <- c(catch,basename(output))
+      name<-output}
+    return(paste(rev(catch),collapse = "/"))
+  }
+  if(feature_num<2){
+    names(data)[2:length(names(data))]<- names(data)[2:length(names(data))] %>%
+      basename() %>%
+      str_replace_all(pattern =  removal, replace = "")
 
-  names(data)[2:length(names(data))]<- names(data)[2:length(names(data))] %>%
-    basename() %>%
-    str_replace_all(pattern =  removal, replace = "")
+    names(data)[1] <- "filename"
+    return(data)
+  }else{
 
-  names(data)[1] <- "filename"
-  return(data)
+    names(data)[2:length(names(data))]<- unlist(lapply(X = names(data)[2:length(names(data))],FUN = renamer,feature_num = feature_num))%>%
+      str_replace_all(pattern =  removal, replace = "")
+
+    names(data)[1] <- "filename"
+    return(data)}
 }
